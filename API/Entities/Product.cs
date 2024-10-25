5be5f0cb873b4c8e06e5f61c8b0e703772f5da07
Susie Douang <soutsada.d@gmail.com>
2024-10-24 20:28:39 -0500

    initial EF data migration

--- API/Entities/Product.cs
+++ API/Entities/Product.cs
@@ -7,7 +7,8 @@ namespace API.Entities
         public string Description { get; set; }
         public long Price { get; set; }
         public string Type { get; set; }
-        public int QuantityInStock {get; set; }
-        
+        public string Brand { get; set; }
+        public int QuantityInStock { get; set; }
+
     }
 }
