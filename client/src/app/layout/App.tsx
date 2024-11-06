e3c5210fb05c99aef00e6f863d7e792c49b733e5
Susie Douang <soutsada.d@gmail.com>
2024-11-06 14:17:13 -0600

    add dark/light mode theme change btn

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,23 +1,41 @@
-import { Container, CssBaseline } from '@mui/material';
+import { Container, createTheme, CssBaseline, FormControlLabel, Switch, ThemeProvider } from '@mui/material';
+import { useState } from 'react';
 import Catalog from '../../components/catalog/Catalog';
 import './App.css';
 import Header from './Header';
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+  const paletteType = darkMode ? 'dark' : 'light';
 
+function handleThemeChange()  {
+  setDarkMode(!darkMode);
+}
+
+  const theme = createTheme({
+    palette: {
+      mode: paletteType,
+    }
+  })
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <div>
-        <CssBaseline/>
+        <CssBaseline />
         <div>
-          <Header/>
-          <Container>          
+          <Header />
+          <FormControlLabel 
+          sx={{ p: 3, mr: 2 }} 
+          control={<Switch checked={darkMode} onChange={handleThemeChange}/>} 
+          label={darkMode ? 
+          'Dark Mode' : 'Light Mode'} />
+          <Container>
             <Catalog />
-            </Container>
+          </Container>
         </div>
       </div>
-    </>
+    </ThemeProvider>
   )
 }
 
-export default App
+export default App;
+
